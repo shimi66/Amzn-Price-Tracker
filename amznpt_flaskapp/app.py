45ca8664fc3bf12d15eba46ae256d4f0b15ee2e5
Erik Voss <87777157+shimi66@users.noreply.github.com>
2024-08-10 22:48:47 -0700

    Basic Timeseries Info Rendering
    On search, the website will now render a time series graph of demo x and y values using Chart.js and Moment.js

--- amznpt_flaskapp/app.py
+++ amznpt_flaskapp/app.py
@@ -1,26 +1,22 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+import random
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
 @app.route('/', methods=['Get', 'Post'])
-def hello():
+def home():
     query = request.args.get('query')
-    results = None
+    results = []
     if query:
         # todo search database and store results
 
-        # temporary values for prototyping
-        results = [f"Result {i} for '{query}'" for i in range(1, 6)]
+        # Example: generating random time series data for demo
+        start_date = datetime.now() - timedelta(days=10)
+        results = [{'date': (start_date + timedelta(days=i)).strftime('%Y-%m-%d'),
+                    'value': random.randint(50, 150)} for i in range(10)]
 
     return render_template('home.html', query=query, results=results)
-
-@app.route('/search')
-def search():
-    query = request.args.get('query')
-    # todo search database
-
-    # temporary return value
-    return f"Search results for: {query}"
 
 if __name__ == "__main__":
     app.run(debug=True)
