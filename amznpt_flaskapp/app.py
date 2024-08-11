5e40bb84cdf090506eec8dbe4973ebfacdb5e162
Erik Voss <87777157+shimi66@users.noreply.github.com>
2024-08-10 18:09:57 -0700

    Adding Search Bar
    Added search bar to home page and updated home route to display results of search on the same page

--- amznpt_flaskapp/app.py
+++ amznpt_flaskapp/app.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['Get', 'Post'])
 def hello():
-    return render_template('home.html')
+    query = request.args.get('query')
+    results = None
+    if query:
+        # todo search database and store results
+
+        # temporary values for prototyping
+        results = [f"Result {i} for '{query}'" for i in range(1, 6)]
+
+    return render_template('home.html', query=query, results=results)
+
+@app.route('/search')
+def search():
+    query = request.args.get('query')
+    # todo search database
+
+    # temporary return value
+    return f"Search results for: {query}"
 
 if __name__ == "__main__":
     app.run(debug=True)
