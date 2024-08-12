e2210da0d70c05268c4781aa95c2b504f759e98a
Erik Voss <87777157+shimi66@users.noreply.github.com>
2024-08-11 17:49:09 -0700

    Added table to home page that is replaced with previous timeseries graph

--- amznpt_flaskapp/app.py
+++ amznpt_flaskapp/app.py
@@ -6,17 +6,18 @@ app = Flask(__name__)
 
 @app.route('/', methods=['Get', 'Post'])
 def home():
-    query = request.args.get('query')
-    results = []
-    if query:
-        # todo search database and store results
+    #render the HTML page
+    return render_template('home.html')
 
-        # Example: generating random time series data for demo
-        start_date = datetime.now() - timedelta(days=50)
-        results = [{'date': (start_date + timedelta(days=i)).strftime('%Y-%m-%d'),
-                    'value': random.randint(50, 150)} for i in range(50)]
-
-    return render_template('home.html', query=query, results=results)
+@app.route('/data')
+def data():
+    # Placeholder time series data
+    start_date = datetime.now() - timedelta(days=50)
+    placeholder_data = {
+        'labels': [(start_date + timedelta(days=i)).strftime('%Y-%m-%d') for i in range(50)],
+        'dataValues': [random.randint(50, 150) for i in range(50)]
+    }
+    return jsonify(placeholder_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
